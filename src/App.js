ee5bf3452f25fe0e52ce137ca04af1279c182f7a
하영 이 <gkdudp9@naver.com>
2022-03-02 23:29:58 +0900

    Tailwind css로 스타일링 바꿉니다.

--- src/App.js
+++ src/App.js
@@ -30,12 +30,11 @@ export default function App() {
   };
 
   return (
-    <div className="container">
-      <div className="todoBlock">
-        <div className="title">
+    <div className="flex items-center justify-center w-screen h-screen bg-blue-100">
+      <div className="w-full p-6 m-4 bg-white rounded shadow lg:w-3/4 lg:max-w-lg">
+        <div className="flex justify-between mb-3">
           <h1>할일 목록</h1>
         </div>
-
         <Lists todoData={todoData} setTodoData={setTodoData}/>
 
         <Form handleSubmit={handleSubmit} value={value} setValue={setValue} />
